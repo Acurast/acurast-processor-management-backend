8c407ee56d0af7d80be9ae7535defcfc1e18f3ec
Andreas Gassmann <andreas@andreasgassmann.ch>
2025-04-22 06:22:40 -0300

    feat: add postgres config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,31 +4,16 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProcessorModule } from './processor/processor.module';
-import { DeviceStatus } from './processor/entities/device-status.entity';
-import { Processor } from './processor/entities/processor.entity';
-import { NetworkType } from './processor/entities/network-type.entity';
-import { BatteryHealth } from './processor/entities/battery-health.entity';
-import { Ssid } from './processor/entities/ssid.entity';
-import { TemperatureReading } from './processor/entities/temperature-reading.entity';
 import { CacheService } from './processor/cache.service';
 import { WhitelistModule } from './whitelist/whitelist.module';
+import { getTypeormConfig } from './db/typeorm.config';
+import { DeviceStatus } from './processor/entities/device-status.entity';
+import { Processor } from './processor/entities/processor.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [
-        DeviceStatus,
-        Processor,
-        NetworkType,
-        BatteryHealth,
-        Ssid,
-        TemperatureReading,
-      ],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(getTypeormConfig()),
     TypeOrmModule.forFeature([DeviceStatus, Processor]),
     ProcessorModule,
     WhitelistModule,
---
+++ src/db/typeorm.config.ts
@@ -0,0 +1,59 @@
+import 'dotenv/config';
+import { DataSource } from 'typeorm';
+import { DataSourceOptions } from 'typeorm/data-source/DataSourceOptions';
+import { DeviceStatus } from '../processor/entities/device-status.entity';
+import { NetworkType } from '../processor/entities/network-type.entity';
+import { Processor } from '../processor/entities/processor.entity';
+import { BatteryHealth } from '../processor/entities/battery-health.entity';
+import { TemperatureReading } from '../processor/entities/temperature-reading.entity';
+import { Ssid } from '../processor/entities/ssid.entity';
+
+export const getTypeormConfig = (): DataSourceOptions => {
+  const entities = [
+    DeviceStatus,
+    Processor,
+    NetworkType,
+    BatteryHealth,
+    Ssid,
+    TemperatureReading,
+  ];
+
+  const sharedConfig = {
+    migrations: ['./dist/db/migrations/*.js'],
+  };
+
+  if (process.env.ENVIRONMENT === 'local') {
+    if (!process.env.DB) {
+      throw new Error('DB environment variable is required');
+    }
+    return {
+      ...sharedConfig,
+      type: 'sqlite',
+      database: process.env.DB /* ?? ':memory:' */,
+      // synchronize: true,
+      entities: entities,
+      logging: false,
+      // dropSchema: true,
+    };
+  }
+
+  return {
+    ...sharedConfig,
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 5432,
+    password: process.env.DB_PASSWORD,
+    username: process.env.DB_USER,
+    entities: entities,
+    database: process.env.DB_NAME,
+    // logging: ['query', 'error'],
+    // logger: 'simple-console',
+    maxQueryExecutionTime: 500,
+    // dropSchema: true,
+    // synchronize: true,
+  };
+};
+
+export const datasource = new DataSource({
+  ...getTypeormConfig(),
+});
